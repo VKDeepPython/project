4dee4df4db70414d91c0bbcaf3f9c980f8bcb913
TimofeyTst <yarik.mist@gmail.com>
2023-11-26 14:46:08 +0300

    abstract

--- responses.py
+++ responses.py
@@ -1,5 +1,23 @@
-class JSONResponse:
+from abc import ABC, abstractmethod
+import json
+
+class BaseResponse(ABC):
+    header = 'text/plain'
+
+    @abstractmethod
+    def __init__(self, response):
+        pass
+
+    @abstractmethod
+    def get_bytes_response(self):
+        pass
+
+class JSONResponse(BaseResponse):
     header = 'application/json'
 
     def __init__(self, response) -> None:
-        pass
+        self.response = response
+
+    def get_bytes_response(self):
+        response_json = json.dumps(self.response)
+        return response_json.encode('utf-8')
