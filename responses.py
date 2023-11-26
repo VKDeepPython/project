10e1843aa8f690e655487d176b39df82022d6c27
TimofeyTst <yarik.mist@gmail.com>
2023-11-26 14:51:15 +0300

    v0.1

--- responses.py
+++ responses.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 import json
 
-class BaseResponse(ABC):
+class TextResponse(ABC):
     header = 'text/plain'
 
     @abstractmethod
@@ -12,12 +12,27 @@ class BaseResponse(ABC):
     def get_bytes_response(self):
         pass
 
-class JSONResponse(BaseResponse):
+class JSONResponse(TextResponse):
     header = 'application/json'
 
-    def __init__(self, response) -> None:
-        self.response = response
+    def __init__(self, response):
+        try:
+            self.response = json.dumps(json.loads(response))
+        except json.JSONDecodeError as err:
+            raise ValueError('invalid JSON') from err
 
     def get_bytes_response(self):
         response_json = json.dumps(self.response)
         return response_json.encode('utf-8')
+    
+class HTMLResponse(TextResponse):
+    header = 'text/html'
+    
+    def init(self, response):
+        self.response = response
+
+class TextResponse(TextResponse):
+    header = 'text/plain'
+    
+    def __init__(self, response):
+        self.response = response
